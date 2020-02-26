3f63c02ef7ecdbd769c6e746a78e9d4b9c85a814
mila-mamat <59339564+mila-mamat@users.noreply.github.com>
2020-02-25 20:51:25 -0500

    Alert if no types are choosen
    Added a new alert code

--- Develop/script.js
+++ Develop/script.js
@@ -10,21 +10,26 @@ function writePassword() {
     let upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     let lowerCase = "abcdefghijklmnopqrstuvwxyz";
     let numeric = "1234567890";
-    let specialChar = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
+    let specialChar = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~';
     let typeRequested = [];
-    var typeIncluded = [];
-    console.log(specialChar)
+    let typeIncluded = [];
+    
     //get the length of password, will keep asking until get a number between 8-128
     let passWordLength;
     do {
       passWordLength = prompt("Please enter a number between 8 and 128 \nHow many characters would you like to have?");
-    } while (passWordLength < 8 || passWordLength >128 || isNaN(passWordLength)); //repeat if it is lower than 8 or more than 128 or not a number
+    } while (passWordLength < 8 || passWordLength >128 || isNaN(passWordLength));
 
     // get the types of characters to be included
     confirm("Would you like to include uppercase letters?") ? typeRequested.push(upperCase):"";
     confirm("Would you like to include lowercase letters?") ? typeRequested.push(lowerCase):"";
     confirm("Would you like to include numbers?") ? typeRequested.push(numeric):"";
     confirm("Would you like to include special characters?") ? typeRequested.push(specialChar):"";
+    
+    // alart if no types are choosen 
+    if (typeRequested.length == 0) {
+      alert("Please choose at least one type of characters.");
+    }
 
     //define password generator function
     function generator() {
@@ -36,11 +41,12 @@ function writePassword() {
       for (let i=1; i<=passWordLength;i++) {
         //randomly choose a type from requested, and mark as included
         randomType = typeRequested[Math.floor(Math.random() * typeRequested.length)];
-        typeIncluded.includes(randomType) ? "":typeIncluded.push(randomType)
+        typeIncluded.includes(randomType) ? "":typeIncluded.push(randomType);
+
         //randomly choose password from the type above
-        passWord += randomType[Math.floor(Math.random()* randomType.length)]
+        passWord += randomType[Math.floor(Math.random()* randomType.length)];
       } 
-      return (passWord)
+      return (passWord);
     } 
 
     // run the generator till all the types requested are included in the password
@@ -48,7 +54,7 @@ function writePassword() {
       generator();
     } 
     
-    return (passWord)
+    return (passWord);
   }
   passwordText.value = password;
 }
